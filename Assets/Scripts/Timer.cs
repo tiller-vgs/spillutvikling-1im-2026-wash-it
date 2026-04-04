9a6bcd59345260bf77ed0915d67221f3bee64002
Teabelly24 <king.teabelly@gmail.com>
2026-04-05 01:25:14 +0200

    Made timer in office room branch because i wasnt ablle to get timer script from timer branch too office room branch
    Timer that goes up

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public float StartTime;
+    public Text timer;
+
+    void Update()
+    {
+        StartTime += Time.deltaTime * 1;
+        int seconds = (int)(StartTime % 60);
+        int minutes = (int)(StartTime / 60) % 60;
+        int hours = (int)(StartTime / 3600) % 24;
+        string timerstr = string.Format("{0:0}:{1:00}:{2:00}", hours, minutes, seconds);
+
+        timer.text = timerstr;
+    }
+}
